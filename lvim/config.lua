43b7b24aa9442bdb210d4ef22baf7d0825524891
dorian-li <49279922+dorian-li@users.noreply.github.com>
2023-04-18 19:52:55 +0800

    fix(lvim): replace clipboard to win32yank

--- lvim/config.lua
+++ lvim/config.lua
@@ -237,16 +237,3 @@ if not dap_ok then
 	return
 end
 dapgo.setup()
-
-vim.g.clipboard = {
-	name = "WslClipboard",
-	copy = {
-		["+"] = "/mnt/c/Windows/System32/clip.exe",
-		["*"] = "/mnt/c/Windows/System32/clip.exe",
-	},
-	paste = {
-		["+"] = '/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-		["*"] = '/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-	},
-	cache_enabled = 0,
-}
